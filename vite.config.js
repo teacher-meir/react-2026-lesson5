fc1d23b532445c9586fa7a95ff76b4426a14f52a
programming-teacher <adi.programming.data@gmail.com>
2025-11-25 01:09:59 +0200

    fix: update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/teacher-meir/',
+    base: '/react-2026-lesson5/',
     plugins: [react()],
 })
